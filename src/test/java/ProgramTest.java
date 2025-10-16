4743795959625b1caf2fd0ef3fd20dfe46d27774
toorchinsky <denisturukin@icloud.com>
2025-10-16 17:31:44 +0400

    юнит-тесты на testng

--- src/test/java/ProgramTest.java
+++ src/test/java/ProgramTest.java
@@ -1,84 +1,70 @@
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class ProgramTest {
 
     @Test
-    @DisplayName("Тест вычисления факториала")
-    void factorialPositive(){
+    public void factorialPositive() {
         long actual = Calculations.factorial(10);
-        assertEquals(3628800, actual);
+        Assert.assertEquals(actual, 3628800);
     }
 
     @Test
-    @DisplayName("Тест 0 факториала")
-    void factorialZero(){
+    public void factorialZero() {
         long actual = Calculations.factorial(0);
-        assertEquals(1, actual);
+        Assert.assertEquals(actual, 1);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void factorialNegative() {
+        Calculations.factorial(-10);
     }
 
     @Test
-    @DisplayName("Тест отрицательного вычисления факториала")
-    void factorialNegative(){
-        assertThrows(IllegalArgumentException.class, () -> Calculations.factorial(-10));
-    }
-
-    @Test
-    @DisplayName("Тест вычисления площади треугольника")
-    void triangleAreaPositive(){
+    public void triangleAreaPositive() {
         int actual = Calculations.triangleArea(14, 14);
-        assertEquals(98, actual);
+        Assert.assertEquals(actual, 98);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void triangleAreaZero() {
+        Calculations.triangleArea(0, 0);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void triangleAreaHalfZero() {
+        Calculations.triangleArea(5, 0);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void triangleAreaNegative() {
+        Calculations.triangleArea(-15, -15);
     }
 
     @Test
-    @DisplayName("Тест на 0 значения при вычислении площади треугольника")
-    void triangleAreaZero(){
-        assertThrows(IllegalArgumentException.class, () -> Calculations.triangleArea(0, 0));
-    }
-
-    @Test
-    @DisplayName("Тест на частино 0 значения при вычислении площади треугольника")
-    void triangleAreaHalfZero(){
-        assertThrows(IllegalArgumentException.class, () -> Calculations.triangleArea(5, 0));
-    }
-
-    @Test
-    @DisplayName("Тест на отрицательные значения при вычислении площади треугольника")
-    void triangleAreaNegative(){
-        assertThrows(IllegalArgumentException.class, () -> Calculations.triangleArea(-15, -15));
-    }
-
-    @Test
-    @DisplayName("Тест на положительные значения массива")
-    void arrayPositive(){
+    public void arrayPositive() {
         int[] actual = Calculations.arithmeticOperations(50, 50);
-        assertArrayEquals(new int[]{100, 0, 1, 2500}, actual);
+        Assert.assertEquals(actual, new int[]{100, 0, 1, 2500});
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void arrayZero() {
+        Calculations.arithmeticOperations(0, 1);
     }
 
     @Test
-    @DisplayName("Тест на операции с 0")
-    void arrayZero(){
-        assertThrows(IllegalArgumentException.class, () -> Calculations.arithmeticOperations(0, 1));
-    }
-
-    @Test
-    @DisplayName("Тест на отрицательные значения массива")
-    void arrayNegative(){
+    public void arrayNegative() {
         int[] actual = Calculations.arithmeticOperations(-50, -50);
-        assertArrayEquals(new int[]{-100, 0, 1, 2500}, actual);
+        Assert.assertEquals(actual, new int[]{-100, 0, 1, 2500});
     }
 
     @Test
-    @DisplayName("Положительное сравнение чисел")
-    void comparisonTrue(){
-        assertTrue(Calculations.comparison(100, 100));
+    public void comparisonTrue() {
+        Assert.assertTrue(Calculations.comparison(100, 100));
     }
 
     @Test
-    @DisplayName("Отрицательное сравнение чисел")
-    void comparisonFalse(){
-        assertFalse(Calculations.comparison(-100, 100));
+    public void comparisonFalse() {
+        Assert.assertFalse(Calculations.comparison(-100, 100));
     }
 }
